2c13c018cb3d29463f348fcecdad2f717c684993
Flybbit <flybbit@gmail.com>
2017-09-12 23:32:57 +0100

    configs names chaged configs alls fixed

--- setup/usb/install.cmd
+++ setup/usb/install.cmd
@@ -1,2 +1,3 @@
-@call %~dp0/../../config/set_apk_info
+@call %~dp0/../../config/setup
+
 @call %adb_cmd_tools%/adb_system_app_push com.flybbit.remotecontrol %REMOTE_CONTROL_APK_SRC_PATH% %REMOTE_CONTROL_APK_DST_NAME%
